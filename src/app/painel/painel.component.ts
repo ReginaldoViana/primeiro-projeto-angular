cb40bcfb4be9602d45b8b0b8f078c770292af4e6
Reginaldo Viana <reginaldo_stz@outlook.com>
2020-03-30 08:26:16 -0300

    Projeto completo

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,15 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { Frase } from '../shared/frase.model'
+import { FRASES } from './frases-mock'
 
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
   styleUrls: ['./painel.component.css']
 })
-export class PainelComponent implements OnInit {
+export class PainelComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public frases:Frase[] = FRASES
+  public instrucao:string = 'Traduza a frase:'
+  public resposta:string = ''
+
+  public rodada:number = 0
+  public rodadaFrase:Frase
+  public progresso:number = 0
+  public tentativas:number = 3
+
+  @Output() public encerrarJogo: EventEmitter<string> = new EventEmitter()
+
+  constructor() {
+    this.atualizaRodada()
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void{}
+
+  public atualizarResposta(resposta: Event):void{
+    this.resposta = (<HTMLInputElement>resposta.target).value
+  }
+
+  public verificarResposta():void{
+
+    if(this.rodadaFrase.frasePtbr == this.resposta){
+      //Troca a pergunta
+      this.rodada++
+
+      //Barra de progresso
+      this.progresso += (100 / this.frases.length)
+
+      //Caso de vitória
+      if(this.rodada === 4){
+        this.encerrarJogo.emit('vitória')
+      }
+
+      //Atualiza o objeto rodadaFrase
+      this.atualizaRodada()
+
+    
+      alert('Acertou a resposta')
+    } else {
+      //Diminuir a variavel tentativas
+      this.tentativas--
+
+      if(this.tentativas === -1){
+        this.encerrarJogo.emit('derrota')
+      }
+    }
+  }
+
+  public atualizaRodada():void{
+
+    //Define a afrase da rodada com base em alguma lógica
+    this.rodadaFrase = this.frases[this.rodada]
+
+    //Limpar campo de texto
+    this.resposta = ''
+
   }
 
 }
